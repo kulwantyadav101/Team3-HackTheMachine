7365a09d3977c7c15e9b74b2cc14f4d9b0c6fd27
Sonali <sonali.chawla@cygrp.com>
2019-03-26 16:00:26 +0530

    Added code for sample intent handler

--- index.js
+++ index.js
@@ -7,10 +7,27 @@ const Alexa = require('ask-sdk-core');
 // Code for the handlers here //
 ////////////////////////////////
 
+
+//The LaunchRequest event occurs when the skill is invoked without a specific intent.
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        const speechText = 'Welcome to the Alexa Skills Kit, you can say hello!';
+return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(speechText)
+            .withSimpleCard('First Alexa handler', speechText)
+            .getResponse();
+    }
+};
+
 exports.handler = Alexa.SkillBuilders.custom()
      .addRequestHandlers(LaunchRequestHandler,
-                         HelloWorldIntentHandler,
                          HelpIntentHandler,
                          CancelAndStopIntentHandler,
                          SessionEndedRequestHandler)
      .lambda();
+
+     
